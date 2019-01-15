f893f6deaaeb20114d21fb6e900a0c66087fff0b
Elias Luhr <e.luhr@herzogkommunikation.de>
2019-01-15 14:47:57 +0100

    use classes & WIP functionality

--- src/widgets/Toolbar.php
+++ src/widgets/Toolbar.php
@@ -11,7 +11,9 @@ namespace dmstr\modules\backend\widgets;
 
 
 use dmstr\modules\backend\assets\ToolbarAsset;
+use pheme\settings\models\Setting;
 use yii\base\Widget;
+use yii\helpers\Url;
 use yii\web\View;
 
 class Toolbar extends Widget
@@ -20,7 +22,7 @@ class Toolbar extends Widget
 
     public function init()
     {
-        $file = \Yii::$app->assetManager->publish(__DIR__.'/../assets/toolbar/js/check-frame.js');
+        $file = \Yii::$app->assetManager->publish(dirname(__DIR__).'/assets/toolbar/js/check-frame.js');
         $this->view->registerJsFile($file[1], ['position'=>View::POS_BEGIN]);
     }
 
@@ -28,5 +30,27 @@ class Toolbar extends Widget
     {
         ToolbarAsset::register($this->view);
         return $this->render('toolbar.twig', ['useIframe'=>$this->useIframe]);
+    }
+
+    /**
+     * @param $section
+     * @return string
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function seoSettingUrl($section) {
+
+        $route = Url::toRoute('');
+        /** @var Setting|null $setting */
+        $setting = Setting::find()->andWhere(['key' => $route,'section' => $section])->one();
+
+        if ($setting === null) {
+            return Url::to(['/settings/default/create','Setting' => [
+                'key' => $route,
+                'section' => $section
+            ]]);
+        }
+
+        return  Url::to(['/settings/default/update','id' => $setting->id]);
+
     }
 }
