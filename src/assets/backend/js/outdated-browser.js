9ae9cfe78b297a86e3113e9bfe052e8e570ddd92
Elias Luhr <e.luhr@herzogkommunikation.de>
2019-04-18 08:59:48 +0200

    fixed js

--- src/assets/backend/js/outdated-browser.js
+++ src/assets/backend/js/outdated-browser.js
@@ -1,12 +1,15 @@
 
 window.addEventListener("load", function () {
+  var outdatedBrowserButton = document.getElementById("buttonCloseUpdateBrowser");
   if (sessionStorage.outdated_browser_confirmed  === "1") {
     document.getElementById("outdated").remove();
   } else {
-    document.getElementById("buttonCloseUpdateBrowser").addEventListener("mousedown", function (e) {
-      e.preventDefault();
-      sessionStorage.outdated_browser_confirmed = "1";
-    });
+    if (outdatedBrowserButton !== null) {
+      outdatedBrowserButton.addEventListener("mousedown", function (e) {
+        e.preventDefault();
+        sessionStorage.outdated_browser_confirmed = "1";
+      });
+    }
   }
 });
 
