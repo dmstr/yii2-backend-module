05e1347acb465c1415d1a38d2ca1985479c2eac9
Tobias Munk <schmunk@usrbin.de>
2023-11-28 14:25:33 +0100

    added minimalistic login layout

---
+++ src/views/layouts/login.php
@@ -0,0 +1,48 @@
+<?php
+// prepare assets
+use dmstr\modules\backend\assets\BackendAsset;
+use dmstr\modules\backend\assets\BackendJsAsset;
+use yii\helpers\Html;
+
+BackendAsset::register($this);
+BackendJsAsset::register($this);
+?>
+
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+    <?= Html::csrfMetaTags() ?>
+  <title><?= Html::encode($this->title) . ' - ' . Yii::$app->name ?></title>
+  <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+  <!-- Theme style -->
+    <?php $this->head() ?>
+</head>
+
+<body class="hold-transition login-page">
+<?php $this->beginBody() ?>
+<div class="login-box">
+  <div class="login-logo">
+    <a href="/"><b><?= getenv('APP_TITLE') ?></b></a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <div class="row">
+      <div class="col-md-offset-3 col-md-6">
+          <?= \dmstr\widgets\Alert::widget() ?>
+      </div>
+    </div>
+
+      <?= $content ?>
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
+
